958dcf004b6af6c63ef6e6b4e1d526bf5edfce62
manickavasagan-s <manickavasagan.s2024lcse@sece.ac.in>
2026-01-14 10:43:58 +0530

    Fix the server proplem

--- server/index.ts
+++ server/index.ts
@@ -84,7 +84,7 @@ app.use((req, res, next) => {
   }
 
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, X-Browser-ID');
   res.header('Access-Control-Allow-Credentials', 'true');
 
   // Security headers
